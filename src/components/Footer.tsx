738c1bef2c19bef5e610499e7c6e33f19d47bdcf
Ed Bates <ebates@zedsbrain.com>
2025-01-04 17:52:31 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full py-6 mt-12 text-center text-gray-600">
       <p className="text-sm">
         Copyright © 2024 - 2025 Ed Bates (TECHBLIP LLC)<br />
-        This software is released under the MIT License. See the LICENSE file for details
+        This software is released under the Apache-2.0 license. See the LICENSE file for details
       </p>
     </footer>
   );
